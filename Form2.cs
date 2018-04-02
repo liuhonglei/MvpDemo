53202d816b7b241fd263316af70280c2f4a882b6
liuhonglei <233624406@qq.com>
2018-04-02 16:26:08 +0800

    Initial commit

---
+++ Form2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MVPDemo
+{
+    public partial class Form2 : Form,IView
+    {
+        private IPresenter _presenter;
+        public Form2()
+        {
+            InitializeComponent();
+            setPresenter(new PresenterImpl(this));
+        }
+
+        public void setPresenter(IPresenter presenter)
+        {
+            this._presenter = presenter;
+        }
+        void IView.showTitle(string title)
+        {
+            buttonEdit1.Text = title;
+        }
+
+        public event EventHandler ButtonSubmitEvent;
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            if (ButtonSubmitEvent != null)
+                ButtonSubmitEvent(sender,e);
+        }
+
+        
+        
+    }
+}
---
+++ View/IView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVPDemo
+{
+    public interface IView
+    {
+        //void setPresenter(IPresenter presenter);
+        void showTitle(string title);
+        event EventHandler ButtonSubmitEvent;  
+    }
+}
