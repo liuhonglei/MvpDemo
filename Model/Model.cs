94b1994a4cda5c892e28c08149e7fbce092508c2
liuhonglei <233624406@qq.com>
2018-04-02 16:31:35 +0800

    Revert "Revert "Initial commit""
    This reverts commit 6c0f378705b92a98263f71ca8bc9d66a7cf5be54.

---
+++ Model/Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVPDemo
+{
+    public class Model:IModel
+    {
+
+        public string GetTitle() {
+
+            return "MVPTest";
+        }
+    }
+}
---
+++ Presenter/PresenterImpl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVPDemo
+{
+    public class PresenterImpl:IPresenter
+    {
+        private IView _view;
+        private IModel _model;
+        public PresenterImpl(IView view) {
+            _view = view;
+            _view.ButtonSubmitEvent += _view_ButtonSubmitEvent;
+            _model = new Model();
+        }
+
+        //void IPresenter.Submit()
+        //{
+        //    if(_view != null)
+        //        _view.ButtonSubmitEvent +=_view_ButtonSubmitEvent;
+        //}
+
+        private void _view_ButtonSubmitEvent(object sender, EventArgs e)
+        {
+            string title = _model.GetTitle();
+            _view.showTitle(title);
+        }
+    }
+}
